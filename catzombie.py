289ace040977af8007328cd3b6f52c3a8706fe10
asyr <50852479+asyrattab@users.noreply.github.com>
2020-11-14 21:33:46 +0800

    Update
    fix cat.png missing

--- catzombie.py
+++ catzombie.py
@@ -3,7 +3,8 @@ from pygame import mixer
 mixer.init()
 
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
 done = False
 #song
 pygame.mixer.music.load("IntroSong.mp3")
@@ -20,38 +21,10 @@ pygame.display.set_icon(icon)
 
 player=pygame.image.load("cat.png").convert()
 cat = pygame.transform.scale(player, (100, 100))
-screen.blit(cat, playerpos)
-keys = [False, False, False, False]
-playerpos=[250,450]
+screen.blit(cat, (250, 350))
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            if event.type == pygame.KEYDOWN:
-                if event.key==K_w:
-                keys[0]=True
-                elif event.key==K_a:
-                keys[1]=True
-                elif event.key==K_s:
-                keys[2]=True
-                elif event.key==K_d:
-                keys[3]=True
-                if event.type == pygame.KEYUP:
-                if event.key==pygame.K_w:
-                keys[0]=False
-                elif event.key==pygame.K_a:
-                keys[1]=False
-                elif event.key==pygame.K_s:
-                keys[2]=False
-                elif event.key==pygame.K_d:
-                keys[3]=False
             done = True
-    if keys[0]:
-        playerpos[1]-=5
-    elif keys[2]:
-        playerpos[1]+=5
-    if keys[1]:
-        playerpos[0]-=5
-    elif keys[3]:
-        playerpos[0]+=5
     pygame.display.flip()
